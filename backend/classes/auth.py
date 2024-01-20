4e56d8cc307276b9e72eaf49fcec35be83ccf07c
Xinran Tang <z5300870@ad.unsw.edu.au>
2024-01-20 12:14:18 +0800

    delete auth outside

--- backend/classes/auth.py
+++ backend/classes/auth.py
@@ -1,14 +1,42 @@
-import validation as V
+import tt1_8.backend.classes.validation as V
+from flask import jsonify, request
+from main import app, db, User
 
-def auth_register(first_name, last_name, username, password):
-    V.valid_name
+# auth
+@APP.route("/auth/register", methods=['POST'])
+def register():
+    """Calls the register function from auth.py"""
+    data = request.get_json()
+    password = data['password']
+    V.valid_name(data['first_name'])
+    V.valid_name(data['last_name'])
+    V.valid_username(data['username'])
+    V.valid_password(password)
+    hashed_password = V.generate_token(password)
+    new_user = User(first_name=data['first_name'], last_name=data['last_name'], username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify(new_user.json()), 201
 
 
+@APP.route("/auth/login", methods=['POST'])
+def login():
+    """Calls the login function from auth.py"""
+    data = request.get_json()
+    return dumps(
+        auth.auth_login(
+            data['username'], data['password']
+        )
+    )
 
-    return True
 
-def auth_login(username, password):
-    return True
-
-def auth_logout(token):
-    return True
+@APP.route("/auth/logout", methods=['POST'])
+def logout():
+    """Calls the logout function from auth.py"""
+    data = request.get_json()
+    return dumps(
+        auth.auth_logout(
+            data['token']
+        )
+    )

b2cdd37b2b121bf5a0bf9408554d0442f72cd2ac
Xinran Tang <z5300870@ad.unsw.edu.au>
2024-01-20 14:54:59 +0800

    fix

--- backend/main.py
+++ backend/main.py
@@ -269,43 +269,43 @@ def create_destination():
 # UPDATE DESTINATION
 @app.route("/destination/<string:destination_id>", methods=["PUT"])
 def update_destination(destination_id):
-   destination = Destination.query.filter_by(destination_id=destination_id).first()
-   if destination:
-      data = request.get_json()
-      token = data["token"]
-      decode_token(token)
-      destination.country_id = data["country_id"]
-      destination.cost = data["cost"]
-      destination.name = data["name"]
-      destination.notes = data["notes"]
-      try:
-         db.session.commit()
-      except:
-            return (
-               jsonify({"message": "An error occurred updating the destination."}),
-               500,
-            )
-      return jsonify(destination.json()), 200
-   return jsonify({"message": "destination not found."}), 404
+    destination = Destination.query.filter_by(destination_id=destination_id).first()
+    if destination:
+        data = request.get_json()
+        token = data["token"]
+        decode_token(token)
+        destination.country_id = data["country_id"]
+        destination.cost = data["cost"]
+        destination.name = data["name"]
+        destination.notes = data["notes"]
+    try:
+            db.session.commit()
+         except:
+               return (
+                  jsonify({"message": "An error occurred updating the destination."}),
+                  500,
+               )
+         return jsonify(destination.json()), 200
+      return jsonify({"message": "destination not found."}), 404
 
 # DELETE DESTINATION
 @app.route("/destination/<string:destination_id>", methods=["DELETE"])
 def delete_destination(destination_id):
-   destination = Destination.query.filter_by(destination_id=destination_id).first()
-   if destination:
-      data = request.get_json()
-      token = data["token"]
-      decode_token(token)
-      try:
-         db.session.delete(destination)
-         db.session.commit()
-      except:
-            return (
-               jsonify({"message": "An error occurred deleting the destination."}),
-               500,
-            )
-      return jsonify({"message": "destination deleted."}), 200
-   return jsonify({"message": "destination not found."}), 404
+    destination = Destination.query.filter_by(destination_id=destination_id).first()
+    if destination:
+        data = request.get_json()
+        token = data["token"]
+        decode_token(token)
+    try:
+            db.session.delete(destination)
+            db.session.commit()
+         except:
+               return (
+                  jsonify({"message": "An error occurred deleting the destination."}),
+                  500,
+               )
+         return jsonify({"message": "destination deleted."}), 200
+      return jsonify({"message": "destination not found."}), 404
 
 
 # CREATE ITINERARY

e15cef4fa160e1089e874224387f164b7a33313f
liu-mojia <liumojia.mj@gmail.com>
2024-01-20 12:49:01 +0800

    added dashboard card

--- frontend/src/components/DashboardCards.tsx
+++ frontend/src/components/DashboardCards.tsx
@@ -3,41 +3,52 @@ import { ItineraryDestinations } from '@/types/Itinerary'
 
 const DashboardCard: React.FC<any> = () => {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      {/* <Image src={property.imageUrl} alt={property.imageAlt} /> */}
-
-      <Box p="6">
-        <Box display="flex" alignItems="baseline">
-          <Badge borderRadius="full" px="2" colorScheme="teal">
+    <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Box
+        p="6"
+        height="120px"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        bgImage="url('https://media.istockphoto.com/id/1344771294/photo/happy-family-in-masks-enjoying-travel-together.webp?b=1&s=612x612&w=0&k=20&c=38vWekRjfDJTxPHNm4N-wpgbp-33XOH8JlBL_GvwBlI=')"
+        bgPosition="center"
+        position="relative"
+        overflow="hidden"
+      >
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgGradient="linear(to-b, whiteAlpha.800, transparent)" // Adjust opacity as needed
+        ></Box>
+        <Box display="flex" alignItems="flex-start">
+          <Badge
+            borderRadius="full"
+            px="2"
+            colorScheme="teal"
+            zIndex={2}
+            alignItems="top"
+          >
             New
           </Badge>
           <Box
-            color="gray.500"
+            color="gray.800"
             fontWeight="semibold"
             letterSpacing="wide"
             fontSize="xs"
             textTransform="uppercase"
             ml="2"
+            zIndex={2}
           >
             Title
           </Box>
         </Box>
-
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-          noOfLines={1}
-        ></Box>
-
-        <Divider mt={4} />
-
-        <Box display="flex" mt="2" alignItems="baseline">
-          <Box as="span" ml="2" color="gray.600" fontSize="sm">
-            <p>Country</p>
-            <p>$ Budget</p>
-          </Box>
+      </Box>
+      <Box display="flex" alignItems="baseline" bg="#F5F5F5" p="3">
+        <Box as="span" ml="2" color="gray.800" fontSize="sm">
+          <p>Country</p>
+          <p>$ Budget</p>
         </Box>
       </Box>
     </Box>

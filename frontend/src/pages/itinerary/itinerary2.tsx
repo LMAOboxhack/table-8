a17c3fb9b6107057482de5c87075bed6afa3c6c4
kexinsee99 <seekexin99@gmail.com>
2024-01-20 11:43:37 +0800

    added another itinerary page

---
+++ frontend/src/pages/itinerary/itinerary2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ItineraryPage2 = () => {
+    return (
+        <div>
+            <h1>Itinerary</h1>
+            <p>This is the Itinerary content2.</p>
+        </div>
+    );
+};
+
+export default ItineraryPage2;

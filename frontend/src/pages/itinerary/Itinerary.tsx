75d984b6ab534c3958b4811d9634147d904f597c
Justin Lam, juslam19 <e0552250@u.nus.edu>
2024-01-20 12:47:37 +0800

    abandoned

--- frontend/src/pages/itinerary/Itinerary.tsx
+++ frontend/src/pages/itinerary/Itinerary.tsx
@@ -1,27 +1,27 @@
 import DataTable from '../../components/tables/DataTable';
 import React, { useEffect, useState } from 'react';
-import ItinerarysAPI from '../api/itinerarys/itinerarys';
+import ItinerarysAPI from '../../api/questions/questions';
 import IconWithText from '../../components/content/IconWithText';
 import { BiSolidBook } from 'react-icons/bi';
 import { Button, Flex, Skeleton, Stack } from '@chakra-ui/react';
 import { type ColumnDef, createColumnHelper, type Column } from '@tanstack/react-table';
-import { type ItineraryDataRowData, ItinerarysTableColumns } from '../../utils/itinerarys';
+import { type QuestionDataRowData, QuestionsTableColumns } from '../../utils/itinerarys';
 import { Link } from 'react-router-dom';
 import { AddIcon } from '@chakra-ui/icons';
 
-const Itinerarys: React.FC = () => {
-  const columnHelper = createColumnHelper<ItineraryDataRowData>();
-  const [itineraryList, setItineraryList] = useState<ItineraryDataRowData[]>([]);
+const Questions: React.FC = () => {
+  const columnHelper = createColumnHelper<QuestionDataRowData>();
+  const [questionList, setQuestionList] = useState<QuestionDataRowData[]>([]);
   // Pass setQuestionList as a prop to the QuestionsTableColumns function
-  const itineraryColumns: Array<ColumnDef<ItineraryDataRowData>> = ItinerarysTableColumns(columnHelper, setItineraryList);
+  const questionColumns: Array<ColumnDef<QuestionDataRowData>> = QuestionsTableColumns(columnHelper, setQuestionList);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     if (!isLoaded) {
-      new ItinerarysAPI()
-        .getItinerarys()
-        .then((itinerarys) => {
-          setItineraryList(itinerarys);
+      new QuestionsAPI()
+        .getQuestions()
+        .then((questions) => {
+          setQuestionList(questions);
         })
         .catch(console.error)
         .finally(() => {
@@ -33,19 +33,19 @@ const Itinerarys: React.FC = () => {
   return (
     <Stack paddingX={16} paddingY={8} spacing={6}>
       <Flex justifyContent={'space-between'}>
-        <IconWithText fontSize="lg" fontWeight="bold" text="Itinerary Repository" icon={<BiSolidBook size={20} />} />
-        <Link to="/itinerary/new">
+        <IconWithText fontSize="lg" fontWeight="bold" text="Question Repository" icon={<BiSolidBook size={20} />} />
+        <Link to="/questions/new">
           <Button leftIcon={<AddIcon />} colorScheme="teal">
-            New Itinerary
+            New Question
           </Button>
         </Link>
       </Flex>
       <Skeleton isLoaded={isLoaded}>
-        {itineraryList !== undefined && (
+        {questionList !== undefined && (
           <DataTable
-            columns={itineraryColumns}
-            tableData={itineraryList}
-            getColumnCanGlobalFilter={(column: Column<ItineraryDataRowData>) => column.getCanSort()}
+            columns={questionColumns}
+            tableData={questionList}
+            getColumnCanGlobalFilter={(column: Column<QuestionDataRowData>) => column.getCanSort()}
           />
         )}
       </Skeleton>
@@ -53,4 +53,4 @@ const Itinerarys: React.FC = () => {
   );
 };
 
-export default Itinerarys;
+export default Questions;

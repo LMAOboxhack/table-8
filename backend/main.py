1acdde82ffd806b74cccb542736b18a58f7dea78
unknown <andy.pee.2020@scis.smu.edu.sg>
2024-01-20 13:28:05 +0800

    git conflicts

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 import platform
-# import tt1_8.backend.classes.validation as V
+import tt1_8.backend.classes.validation as V
 import jwt
 
 app = Flask(__name__, static_folder="static")
@@ -110,7 +110,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(1000), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
     username = db.Column(db.String(20), nullable=False)
 
     def init(self, username, first_name, last_name, password):
@@ -265,58 +265,6 @@ def delete_destination(destination_id):
     return jsonify({"message": "destination not found."}), 404
 
 
-# CREATE ITINERARY
-@app.route('/itinerary', methods=['POST'])
-def create_itinerary():
-    data = request.json
-    new_itinerary = Itinerary(country_id=data['country_id'], user_id=data['user_id'], budget=data['budget'], title=data['title'])
-    db.session.add(new_itinerary)
-    db.session.commit()
-    return jsonify(new_itinerary.json()), 201
-
-# GET ALL ITINERARIES
-@app.route('/itinerary', methods=['GET'])
-def get_itineraries():
-    itineraries = Itinerary.query.all()
-    return jsonify([itinerary.json() for itinerary in itineraries])
-
-# GET 1 ITINERARY BASED ON ITINERARY_ID
-@app.route('/itinerary/<itinerary_id>', methods=['GET'])
-def get_itinerary(itinerary_id):
-    itinerary = Itinerary.query.get(itinerary_id)
-    if itinerary:
-        return jsonify(itinerary.json())
-    else:
-        return jsonify({"message": "Itinerary not found"}), 404
-
-# UPDATE ITINERARY BASED ON ITINERARY_ID
-@app.route('/itinerary/<itinerary_id>', methods=['PUT'])
-def update_itinerary(itinerary_id):
-    itinerary = Itinerary.query.get(itinerary_id)
-    if itinerary:
-        data = request.json
-        itinerary.country_id = data['country_id']
-        itinerary.user_id = data['user_id']
-        itinerary.budget = data['budget']
-        itinerary.title = data['title']
-        db.session.commit()
-        return jsonify(itinerary.json())
-    else:
-        return jsonify({"message": "Itinerary not found"}), 404
-
-# DELETE ITINERARY BASED ON ITINERARY_ID
-@app.route('/itinerary/<itinerary_id>', methods=['DELETE'])
-def delete_itinerary(itinerary_id):
-    itinerary = Itinerary.query.get(itinerary_id)
-    if itinerary:
-        db.session.delete(itinerary)
-        db.session.commit()
-        return jsonify({"message": "Itinerary deleted successfully"})
-    else:
-        return jsonify({"message": "Itinerary not found"}), 404
-
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-

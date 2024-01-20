49f4372e932ebfa26b7f97e56f4555a834bba0db
farand77 <farand77@hotmail.com>
2024-01-20 10:19:17 +0800

    added country and destination classes

--- backend/main.py
+++ backend/main.py
@@ -21,19 +21,21 @@ db = SQLAlchemy(app)
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 jwt = JWTManager(app)
 
-class User(db.Model):
-    def __init__(self, id, username, password, mcr, role_id):
-        self.id = id
-        self.username = username
-        self.password = password
-        self.mcr = mcr
-        self.role_id = role_id
-    
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    mcr = db.Column(db.String(7), nullable=True)
-    role_id = db.Column(db.Integer, nullable=False)
+class Country(db.Model):
+   def __init__(self, id, name):
+      self.id = id
+      self.name = name
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(255), nullable=False)
+
+class Destination(db.Model):
+   def __init__(self, id, country_id, cost, name, notes):
+      self.id = id
+      self.country_id = country_id
+      self.
+      self.name = name
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(255), nullable=False)
 
 
 @app.route('/')

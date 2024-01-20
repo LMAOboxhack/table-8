24f2bd94c0bb38ef14b735b2e9ec9514d782f64c
liu-mojia <liumojia.mj@gmail.com>
2024-01-20 12:14:45 +0800

    added card component

--- frontend/src/components/DashboardCards.tsx
+++ frontend/src/components/DashboardCards.tsx
@@ -1,33 +1,44 @@
-import { Box, Text, Image } from '@chakra-ui/react'
+import { Box, Text, Image, Badge, Divider } from '@chakra-ui/react'
+import { ItineraryDestinations } from '@/types/Itinerary'
 
-interface CardProps {
-  title: string
-  description: string
-  imageUrl: string
-}
-
-const DashboardCard: React.FC<CardProps> = ({
-  title,
-  description,
-  imageUrl,
-}) => {
+const DashboardCard: React.FC<any> = () => {
   return (
-    <Box
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      boxShadow="lg"
-      p={4}
-      maxW="md"
-    >
-      <Image src={imageUrl} alt={title} height="200px" objectFit="cover" />
-      <Box mt={4}>
-        <Text fontSize="xl" fontWeight="semibold">
-          {title}
-        </Text>
-        <Text mt={2} color="gray.500">
-          {description}
-        </Text>
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      {/* <Image src={property.imageUrl} alt={property.imageAlt} /> */}
+
+      <Box p="6">
+        <Box display="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
+            New
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            Title
+          </Box>
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          noOfLines={1}
+        ></Box>
+
+        <Divider mt={4} />
+
+        <Box display="flex" mt="2" alignItems="baseline">
+          <Box as="span" ml="2" color="gray.600" fontSize="sm">
+            <p>Country</p>
+            <p>$ Budget</p>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
